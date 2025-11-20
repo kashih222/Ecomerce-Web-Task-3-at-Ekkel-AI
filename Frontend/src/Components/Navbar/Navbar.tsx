cc1373d7b4c377ff16f9586b88b791424941938f
kashih222 <2021i0346@gmail.com>
2025-11-20 12:10:27 +0500

    login signup authentication done on client side

--- Frontend/src/Components/Navbar/Navbar.tsx
+++ Frontend/src/Components/Navbar/Navbar.tsx
@@ -1,57 +1,106 @@
 import { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { Menu, X } from "lucide-react";
-import logo from '../../assets/Logo.png'
+import logo from "../../assets/Logo.png";
+import { useForm } from "react-hook-form";
+import type { SubmitHandler } from "react-hook-form";
 
+type InputsLogin = {
+  email: string;
+  password: string;
+};
 
-
-
+type InputsSignup = {
+  fullname: string;
+  email: string;
+  password: string;
+};
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
-  const [OpenLogin, setOpenLogin] = useState(false); 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState(""); 
+  const [OpenLogin, setOpenLogin] = useState(false);
   const [openSignup, setOpenSignup] = useState(false);
-  const [name, setName] = useState("");
-  
 
-  const handleLogin = (e: React.FormEvent) => {
-    e.preventDefault();
-  }
+  const delay = (ms: number) =>
+    new Promise((resolve) => setTimeout(resolve, ms));
 
-  const handleSignup = (e: React.FormEvent) => {
-    e.preventDefault();
-  }
+  // LOGIN FORM
+  const {
+    register: registerLogin,
+    handleSubmit: handleSubmitLogin,
+    reset: resetLogin,
+    formState: { errors: loginErrors, isSubmitting: loginSubmitting },
+  } = useForm<InputsLogin>();
 
+  // SIGNUP FORM
+  const {
+    register: registerSignup,
+    handleSubmit: handleSubmitSignup,
+    reset: resetSignup,
+    formState: { errors: signupErrors, isSubmitting: signupSubmitting },
+  } = useForm<InputsSignup>();
+
+  // LOGIN SUBMIT
+  const onLoginSubmit: SubmitHandler<InputsLogin> = async (data) => {
+    await delay(2000);
+    console.log("Login:", data);
+    resetLogin();
+  };
+
+  // SIGNUP SUBMIT
+  const onSignupSubmit: SubmitHandler<InputsSignup> = async (data) => {
+    await delay(2000);
+    console.log("Signup:", data);
+    resetSignup();
+  };
 
   return (
     <nav className="w-full bg-white shadow-md fixed top-0 left-0 z-50">
       <div className="container mx-auto flex items-center justify-between px-4 py-3">
-
-        
         <NavLink to="/" className="">
-          <img src={logo} alt="logo" className="w-[100px] hover:scale-95 duration-400 sm:w-[150px]  md:w-190px lg:w-200px xl:w-[200px]"  />
+          <img
+            src={logo}
+            alt="logo"
+            className="w-[100px] hover:scale-95 duration-400 sm:w-[150px]  md:w-190px lg:w-200px xl:w-[200px]"
+          />
         </NavLink>
 
         {/* Desktop Menu */}
         <ul className="hidden md:flex items-center gap-8 text-gray-700 text-lg font-medium">
-          <li><NavLink to="/" className="hover:text-black  duration-200">Home</NavLink></li>
-          <li><NavLink to="/products" className="hover:text-black duration-200">Products</NavLink></li>
-          <li><NavLink to="/about" className="hover:text-black duration-200">About</NavLink></li>
-          <li><NavLink to="/contact" className="hover:text-black duration-200">Contact</NavLink></li>
+          <li>
+            <NavLink to="/" className="hover:text-black  duration-200">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/products" className="hover:text-black duration-200">
+              Products
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/about" className="hover:text-black duration-200">
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact" className="hover:text-black duration-200">
+              Contact
+            </NavLink>
+          </li>
         </ul>
 
         {/* Desktop Buttons */}
         <div className="hidden md:flex gap-4">
-          <button className="px-5 py-2 border border-black text-black rounded-full hover:bg-black hover:scale-95 hover:text-white duration-300"
-          onClick={() => setOpenLogin(true)}>
+          <button
+            className="px-5 py-2 border border-black text-black rounded-full hover:bg-black hover:scale-95 hover:text-white duration-300"
+            onClick={() => setOpenLogin(true)}
+          >
             Login
-
           </button>
-          <button className="px-5 py-2 bg-black text-white rounded-full hover:scale-95 duration-300"
-          onClick={() => setOpenSignup(true)}>
-          
+          <button
+            className="px-5 py-2 bg-black text-white rounded-full hover:scale-95 duration-300"
+            onClick={() => setOpenSignup(true)}
+          >
             Sign Up
           </button>
         </div>
@@ -66,87 +115,144 @@ const Navbar = () => {
       {open && (
         <div className="md:hidden bg-white shadow-md py-4 px-6 space-y-6">
           <ul className="flex flex-col gap-4 text-gray-700 font-medium">
-            <li><Link to="/" onClick={() => setOpen(false)}>Home</Link></li>
-            <li><Link to="/products" onClick={() => setOpen(false)}>Products</Link></li>
-            <li><Link to="/about" onClick={() => setOpen(false)}>About</Link></li>
-            <li><Link to="/contact" onClick={() => setOpen(false)}>Contact</Link></li>
+            <li>
+              <Link to="/" onClick={() => setOpen(false)}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/products" onClick={() => setOpen(false)}>
+                Products
+              </Link>
+            </li>
+            <li>
+              <Link to="/about" onClick={() => setOpen(false)}>
+                About
+              </Link>
+            </li>
+            <li>
+              <Link to="/contact" onClick={() => setOpen(false)}>
+                Contact
+              </Link>
+            </li>
           </ul>
 
           {/* Mobile Buttons */}
           <div className="flex flex-col gap-3 pt-4">
-            <button className="px-5 py-2 border border-black text-black rounded-full hover:scale-90 hover:text-white duration-300"
-            onClick={() => setOpenLogin(true)}>
+            <button
+              className="px-5 py-2 border border-black text-black rounded-full hover:scale-90 hover:text-white duration-300"
+              onClick={() => setOpenLogin(true)}
+            >
               Login
             </button>
-            <button className="px-5 py-2 bg-black text-white rounded-full hover:scale-90 duration-300"
-            onClick={() => setOpenSignup(true)}>
+            <button
+              className="px-5 py-2 bg-black text-white rounded-full hover:scale-90 duration-300"
+              onClick={() => setOpenSignup(true)}
+            >
               Sign Up
             </button>
           </div>
         </div>
       )}
 
-     
       {OpenLogin && (
-        <div className="fixed inset-0 backdrop-blur-sm bg-opacity-50 flex items-center justify-center z-50">
-          {/* Modal Box */}
-          <div className="bg-white rounded-lg w-11/12 sm:w-96 p-6 relative shadow-lg">
-            {/* Close Button */}
-            <button
-              onClick={() => setOpenLogin(false)}
-              className="absolute top-3 right-3 text-gray-500 hover:text-gray-900"
-            >
-              ✕
-            </button>
-
-            {/* Modal Content */}
-            <h2 className="text-2xl font-bold mb-4 text-center">Login</h2>
-            <form onSubmit={handleLogin} className="flex flex-col gap-4">
-              <input
-                type="email"
-                placeholder="Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-gray-900"
-                required
-              />
-              <input
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-gray-900"
-                required
-              />
+        <>
+          <div className="fixed inset-0 backdrop-blur-sm bg-opacity-50 flex items-center justify-center z-50">
+            <div className="bg-white rounded-lg w-11/12 sm:w-96 p-6 relative shadow-lg">
               <button
-                type="submit"
-                className="bg-gray-900 text-white py-2 rounded-lg hover:bg-gray-700 transition"
+                onClick={() => setOpenLogin(false)}
+                className="absolute top-3 right-3 text-gray-500 hover:text-gray-900"
               >
-                Login
+                ✕
               </button>
-            </form>
 
-            {/* Extra Links */}
-            <p className="text-sm text-gray-500 mt-4 text-center">
-              Don't have an account?{" "}
-              <span
-                className="text-blue-600 cursor-pointer hover:underline"
-                onClick={() => {
-                  setOpenLogin(false);
-                  setOpenSignup(true);}}
+              <h2 className="text-2xl font-bold mb-4 text-center">Login</h2>
+              <form
+                onSubmit={handleSubmitLogin(onLoginSubmit)}
+                className="flex flex-col gap-4"
               >
-                Sign Up
-              </span>
-            </p>
+                <input
+                  type="email"
+                  placeholder="Email"
+                  className="border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-gray-900"
+                  {...registerLogin("email", {
+                    required: true,
+
+                    pattern:
+                      /^[a-zA-Z0-9._%+-]{4,}@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                  })}
+                />
+                <input
+                  type="password"
+                  placeholder="Password"
+                  className="border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-gray-900"
+                  {...registerLogin("password", {
+                    required: true,
+                    minLength: 6,
+                    maxLength: 20,
+                    pattern:
+                      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/,
+                  })}
+                />
+                {(loginErrors.email || loginErrors.password) && (
+                  <div className="text-sm text-red-600 space-y-1 mt-1">
+                    {/* Email Errors */}
+                    {loginErrors.email?.type === "required" && (
+                      <div>Email is required.</div>
+                    )}
+                    {loginErrors.email?.type === "pattern" && (
+                      <div>Enter a valid email address.</div>
+                    )}
+
+                    {/* Password Errors */}
+                    {loginErrors.password?.type === "required" && (
+                      <div>Password is required.</div>
+                    )}
+                    {loginErrors.password?.type === "minLength" && (
+                      <div>Password must be at least 6 characters.</div>
+                    )}
+                    {loginErrors.password?.type === "maxLength" && (
+                      <div>Password must be at most 20 characters.</div>
+                    )}
+                    {loginErrors.password?.type === "pattern" && (
+                      <div>
+                        Password must contain:
+                        <br />– 1 uppercase letter
+                        <br />– 1 lowercase letter
+                        <br />– 1 number
+                        <br />– 1 special character
+                      </div>
+                    )}
+                  </div>
+                )}
+
+                <input
+                  type="submit"
+                  value="Login"
+                  className="bg-gray-900 text-white py-2 rounded-lg hover:bg-gray-700 transition"
+                  disabled={loginSubmitting}
+                />
+              </form>
+
+              <p className="text-sm text-gray-500 mt-4 text-center">
+                Don't have an account?{" "}
+                <span
+                  className="text-blue-600 cursor-pointer hover:underline"
+                  onClick={() => {
+                    setOpenLogin(false);
+                    setOpenSignup(true);
+                  }}
+                >
+                  Sign Up
+                </span>
+              </p>
+            </div>
           </div>
-        </div>
+        </>
       )}
 
-     
-     
       {openSignup && (
         <div className="fixed inset-0 backdrop-blur-sm bg-opacity-50 flex items-center justify-center z-50">
-          {/* Modal Box */}
           <div className="bg-white rounded-lg w-11/12 sm:w-96 p-6 relative shadow-lg">
             {/* Close Button */}
             <button
@@ -156,38 +262,97 @@ const Navbar = () => {
               ✕
             </button>
 
-            {/* Modal Content */}
+            {/* Heading */}
             <h2 className="text-2xl font-bold mb-4 text-center">Sign Up</h2>
-            <form onSubmit={handleSignup} className="flex flex-col gap-4">
+
+            {/* SignUp Form */}
+            <form
+              className="flex flex-col gap-4"
+              onSubmit={handleSubmitSignup(onSignupSubmit)}
+            >
               <input
                 type="text"
                 placeholder="Full Name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
                 className="border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-gray-900"
-                required
+                {...registerSignup("fullname", {
+                  required: "Full Name is required",
+                  minLength: {
+                    value: 3,
+                    message: "Full Name must be at least 3 characters",
+                  },
+                  maxLength: {
+                    value: 30,
+                    message: "Full Name must be at most 30 characters",
+                  },
+                  pattern: {
+                    value: /^[a-zA-Z0-9]+$/,
+                    message: "Full Name can only contain letters and spaces",
+                  },
+                })}
               />
               <input
                 type="email"
                 placeholder="Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
                 className="border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-gray-900"
-                required
+                {...registerSignup("email", {
+                  required: "Email is required",
+                  pattern: {
+                    value:
+                      /^[a-zA-Z0-9._%+-]{4,}@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                    message:
+                      "Enter a valid email with at least 4 chars before @",
+                  },
+                })}
               />
               <input
                 type="password"
                 placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
                 className="border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-gray-900"
-                required
+                {...registerSignup("password", {
+                  required: "Password is required",
+                  minLength: {
+                    value: 6,
+                    message: "Password must be at least 6 characters",
+                  },
+                  maxLength: {
+                    value: 20,
+                    message: "Password must be at most 20 characters",
+                  },
+                  pattern: {
+                    value:
+                      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/,
+                    message:
+                      "Password must contain 1 uppercase, 1 lowercase, 1 number & 1 special char",
+                  },
+                })}
               />
+
+              {/* Error Messages */}
+              {(signupErrors.fullname ||
+                signupErrors.email ||
+                signupErrors.password) && (
+                <div className="text-sm text-red-600 space-y-1 mt-1">
+                  {signupErrors.fullname && (
+                    <div>{signupErrors.fullname.message}</div>
+                  )}
+                  {signupErrors.email && (
+                    <div>{signupErrors.email.message}</div>
+                  )}
+                  {signupErrors.password && (
+                    <div>{signupErrors.password.message}</div>
+                  )}
+                </div>
+              )}
+
+              {/* Submit Button */}
               <button
                 type="submit"
-                className="bg-gray-900 text-white py-2 rounded-lg hover:bg-gray-700 transition"
+                disabled={signupSubmitting}
+                className={`bg-gray-900 text-white py-2 rounded-lg hover:bg-gray-700 transition ${
+                  signupSubmitting ? "opacity-50 cursor-not-allowed" : ""
+                }`}
               >
-                Sign Up
+                {signupSubmitting ? "Submitting..." : "Sign Up"}
               </button>
             </form>
 
@@ -199,7 +364,6 @@ const Navbar = () => {
                 onClick={() => {
                   setOpenLogin(true);
                   setOpenSignup(false);
-                  
                 }}
               >
                 Login
@@ -208,7 +372,6 @@ const Navbar = () => {
           </div>
         </div>
       )}
-     
     </nav>
   );
 };
