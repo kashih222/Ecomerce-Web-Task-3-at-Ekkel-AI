612ece3cb42f86c8f03281fa8e4672fa8929f2cf
kashih222 <2021i0346@gmail.com>
2026-01-19 15:52:23 +0500

    set path in apolloclient uri

--- Frontend/src/GraphqlOprations/apolloClient.ts
+++ Frontend/src/GraphqlOprations/apolloClient.ts
@@ -3,7 +3,7 @@ import { setContext } from "@apollo/client/link/context";
 
 // ✅ GraphQL endpoint from env
 const httpLink = new HttpLink({
-  uri: `${import.meta.env.VITE_BACKEND_PRODUCTION_URL}/graphql`,
+  uri: `${import.meta.env.VITE_BACKEND_PRODUCTION_URL}graphql`,
   credentials: "include",
 });
 
